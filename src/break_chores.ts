fe1a033a5c723252dc5d8018eaa385ea241c7979
Benjamin <benjamin.phillabaum@gmail.com>
2024-04-25 04:26:36 -0400

    Switched out amulet for t-shirt: t-shirt gives 2 in main stat

--- src/break_chores.ts
+++ src/break_chores.ts
@@ -64,7 +64,7 @@ function handle_apriling(use_sax = true): void {
 }
 
 function get_catalog_items(): void {
-    buy(toCoinmaster("Mr. Store 2002"), 1, toItem("Amulet of Perpetual Darkness"));
+    buy(toCoinmaster("Mr. Store 2002"), 1, toItem('"I survived Y2K" T-Shirt'));
     buy(toCoinmaster("Mr. Store 2002"), 1, toItem("Flash Liquidizer Ultra Dousing Accessory"));
     buy(toCoinmaster("Mr. Store 2002"), 1, toItem("pro skateboard"));
     return;
